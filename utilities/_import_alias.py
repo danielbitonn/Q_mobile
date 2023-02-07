6f14ecaea90dc4503256637d583f7598339ffc87
Daniel Biton <47026541+danielbitonn@users.noreply.github.com>
2023-02-07 16:02:53 +0200

    Update _import_alias.py

--- utilities/_import_alias.py
+++ utilities/_import_alias.py
@@ -1,7 +1,7 @@
 ### Basic packages
 import numpy as np
 import pandas as pd
-pd.set_option('display.max_colwidth', 8000)
+# pd.set_option('display.max_colwidth', 8000)
 from numpy.linalg import norm
 import statistics
 from scipy import stats as stat
@@ -14,11 +14,11 @@ import datetime
 from datetime import datetime as dt
 from datetime import timedelta
 import yaml
-from yaml.loader import *
-import pprint
-from beeprint import pp
+# from yaml.loader import *
+# import pprint
+# from beeprint import pp
 import logging
-import pickle
+# import pickle
 # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', filename='/tmp/myapp.log', filemode='w')
 
 ### Files
